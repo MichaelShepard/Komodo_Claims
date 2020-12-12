853eee7cc0ade85a52f7badb58c33162d8c23149
Michael Shepard <mike@rockcastlepartners.com>
2020-12-12 16:19:52 -0500

    setup UI

--- ClaimsRepository/ClaimsRepo.cs
+++ ClaimsRepository/ClaimsRepo.cs
@@ -37,7 +37,7 @@ namespace ClaimsRepository
         }
 
 
-        // Update MIke MIke
+        // Update 
 
 
 
--- Komodo_Claims/ProgramUI.cs
+++ Komodo_Claims/ProgramUI.cs
@@ -14,8 +14,68 @@ namespace ClaimsProgram
             SeedMenuList();
             Menu();
         }
-    
-    
-    
+
+        public void Menu()
+        {
+
+            bool keepRunning = true;
+
+            while (keepRunning)
+            {
+
+                Console.WriteLine("Select an option: \n" +
+                    "1. Create New Menu Item \n" +
+                    "2. View All Menu Items \n" +
+                    "3. View Menu Item By Meal # \n" +
+                    "4. Update Meal Item \n" +
+                    "5. Delete A Meal Item \n" +
+                    "6. Exit");
+
+                string input = Console.ReadLine();
+
+                switch(input)
+                {
+
+                    case "1":
+                        SeeAllClaims();
+                        break;
+                    case "2":
+                        TakeCareOfNextClaim();
+                        break;
+                    case "3":
+                        EnterNewClaim();
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid selection.");
+                        break;
+
+
+                }
+
+                Console.WriteLine("Press any key to conitnue...");
+                Console.ReadKey();
+                Console.Clear();
+
+            }
+        } //End of Menu
+
+        public void SeeAllClaims()
+        {
+
+        }
+
+
+        public void TakeCareOfNextClaim()
+        {
+
+
+        }
+
+        public void EnterNewClaim()
+        {
+
+        }
+
+
     } // end of Program UI Class
 } // End of Namespace
